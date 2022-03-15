9ff503f47198c0e50a0073b71d5d0319a2a54d0f
amadejzr <amadej@jerlah.si>
2022-03-15 12:38:22 +0100

    User

--- lib/models/users.dart
+++ lib/models/users.dart
@@ -12,57 +12,22 @@ class Users {
   Null? mobile;
   String? email;
   String? gender;
-  Null? pictureUri;
-  Null? customId;
-  Null? customField1;
-  Null? customField2;
-  Null? customField3;
-  Null? customField4;
-  Null? customField5;
-  Null? customField6;
-  Null? customField7;
-  Null? customField8;
-  Null? customField9;
-  Null? customField10;
-  bool? isTimeAttendanceUser;
-  bool? isArchived;
-  bool? hasUserAccount;
-  Null? userAccountId;
-  Null? calculationStartDate;
-  Null? calculationStopDate;
 
-  Users(
-      {this.id,
-      this.firstName,
-      this.lastName,
-      this.middleName,
-      this.fullName,
-      this.birthDate,
-      this.address,
-      this.city,
-      this.state,
-      this.phone,
-      this.mobile,
-      this.email,
-      this.gender,
-      this.pictureUri,
-      this.customId,
-      this.customField1,
-      this.customField2,
-      this.customField3,
-      this.customField4,
-      this.customField5,
-      this.customField6,
-      this.customField7,
-      this.customField8,
-      this.customField9,
-      this.customField10,
-      this.isTimeAttendanceUser,
-      this.isArchived,
-      this.hasUserAccount,
-      this.userAccountId,
-      this.calculationStartDate,
-      this.calculationStopDate});
+  Users({
+    this.id,
+    this.firstName,
+    this.lastName,
+    this.middleName,
+    this.fullName,
+    this.birthDate,
+    this.address,
+    this.city,
+    this.state,
+    this.phone,
+    this.mobile,
+    this.email,
+    this.gender,
+  });
 
   Users.fromJson(Map<String, dynamic> json) {
     id = json['Id'];
@@ -78,24 +43,6 @@ class Users {
     mobile = json['Mobile'];
     email = json['Email'];
     gender = json['Gender'];
-    pictureUri = json['PictureUri'];
-    customId = json['CustomId'];
-    customField1 = json['CustomField1'];
-    customField2 = json['CustomField2'];
-    customField3 = json['CustomField3'];
-    customField4 = json['CustomField4'];
-    customField5 = json['CustomField5'];
-    customField6 = json['CustomField6'];
-    customField7 = json['CustomField7'];
-    customField8 = json['CustomField8'];
-    customField9 = json['CustomField9'];
-    customField10 = json['CustomField10'];
-    isTimeAttendanceUser = json['IsTimeAttendanceUser'];
-    isArchived = json['IsArchived'];
-    hasUserAccount = json['HasUserAccount'];
-    userAccountId = json['UserAccountId'];
-    calculationStartDate = json['CalculationStartDate'];
-    calculationStopDate = json['CalculationStopDate'];
   }
 
   Map<String, dynamic> toJson() {
@@ -113,24 +60,7 @@ class Users {
     data['Mobile'] = this.mobile;
     data['Email'] = this.email;
     data['Gender'] = this.gender;
-    data['PictureUri'] = this.pictureUri;
-    data['CustomId'] = this.customId;
-    data['CustomField1'] = this.customField1;
-    data['CustomField2'] = this.customField2;
-    data['CustomField3'] = this.customField3;
-    data['CustomField4'] = this.customField4;
-    data['CustomField5'] = this.customField5;
-    data['CustomField6'] = this.customField6;
-    data['CustomField7'] = this.customField7;
-    data['CustomField8'] = this.customField8;
-    data['CustomField9'] = this.customField9;
-    data['CustomField10'] = this.customField10;
-    data['IsTimeAttendanceUser'] = this.isTimeAttendanceUser;
-    data['IsArchived'] = this.isArchived;
-    data['HasUserAccount'] = this.hasUserAccount;
-    data['UserAccountId'] = this.userAccountId;
-    data['CalculationStartDate'] = this.calculationStartDate;
-    data['CalculationStopDate'] = this.calculationStopDate;
+
     return data;
   }
 }
